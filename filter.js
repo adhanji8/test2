bbc43f02747c5b5bfcedeb76819d5f676016f7a6
adhanji8 <adhanji8@gmail.com>
2020-03-19 16:49:49 -0700

    first commit

---
+++ filter.js
@@ -0,0 +1,9 @@
+function filter(inputArr, searchTerm) {
+    if (!searchTerm) throw Error("searchTerm cannot be empty");
+    if (!inputArr.length) throw Error("inputArr cannot be empty"); // new line
+    return inputArr.filter(function(arrayElement) {
+      return arrayElement.url.toLowerCase().includes(searchTerm.toLowerCase())
+    });
+  }
+  
+  module.exports = filter;
